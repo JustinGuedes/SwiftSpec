59de4fdac3f76bdd678ae8b18e9cb24fa4d034fc
JustinGuedes <justin.guedes@gmail.com>
2017-06-18 17:34:18 +0200

    Add range specification and closed range specification

--- SwiftSpec/Classes/Specifications/ClosedRangeSpecification.swift
+++ SwiftSpec/Classes/Specifications/ClosedRangeSpecification.swift
@@ -5,4 +5,17 @@
 //  Created by Justin Guedes on 2017/06/16.
 //
 
-import Foundation
+/// Specification that allows for closed range (includes upper bound value) checking.
+public struct ClosedRangeSpecification<ComparableType: Comparable>: Specification {
+
+    private let range: ClosedRange<ComparableType>
+
+    public init(_ range: ClosedRange<ComparableType>) {
+        self.range = range
+    }
+
+    public func isSatisfied(by comparable: ComparableType) -> Bool {
+        return range.contains(comparable)
+    }
+
+}
--- SwiftSpec/Classes/Specifications/RangeSpecification.swift
+++ SwiftSpec/Classes/Specifications/RangeSpecification.swift
@@ -1,8 +1,21 @@
 //
 //  RangeSpecification.swift
-//  Pods-SwiftSpec_Example
+//  SwiftSpec
 //
 //  Created by Justin Guedes on 2017/06/16.
 //
 
-import Foundation
+/// Specification that allows for range (excludes upper bound value) checking.
+public struct RangeSpecification<ComparableType: Comparable>: Specification {
+
+    private let range: Range<ComparableType>
+
+    public init(_ range: Range<ComparableType>) {
+        self.range = range
+    }
+
+    public func isSatisfied(by comparable: ComparableType) -> Bool {
+        return range.contains(comparable)
+    }
+
+}
